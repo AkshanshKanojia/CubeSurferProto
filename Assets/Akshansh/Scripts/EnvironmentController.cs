e0ac9cfd5e1dd37a380673c633b6ffa1521701d9
AkshanshKanojia <akshanshkanojia@gmail.com>
2022-12-28 20:23:03 +0530

    Finished environment controller

--- Assets/Akshansh/Scripts/EnvironmentController.cs
+++ Assets/Akshansh/Scripts/EnvironmentController.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnvironmentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] enum ObjectTypes { MovingPlatform,CubePickup,Wall,
+    Coin,Cube}
+    [SerializeField] ObjectTypes BehaviourType;
+    public bool IsActive = true;
+    [SerializeField] float moveSpeed = 3f,minFallDist = -2f;
+    [SerializeField] int cubeBlockLimit = 1,cubesToAdd = 1;
+
+    PlayerController playerCont;
+
+    private void Start()
     {
-        
+        playerCont = FindObjectOfType<PlayerController>();
+    }
+    private void Update()
+    {
+        if(IsActive)
+        {
+            ObjectManager();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //applies reequired actions based on behaviour type
+    void ObjectManager()
     {
-        
+        switch(BehaviourType)
+        {
+            case ObjectTypes.MovingPlatform:
+                MoveObject();
+                break;
+            case ObjectTypes.Cube:
+                if(transform.position.y<minFallDist)
+                {
+                    Destroy(gameObject);
+                }
+                break;
+        }
+    }
+
+    void MoveObject()
+    {
+        transform.position += moveSpeed * Time.deltaTime * -Vector3.forward;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Cubes")&&IsActive)
+        {
+            switch (BehaviourType)
+            {
+                case ObjectTypes.Wall:
+                    collision.transform.parent = transform;
+                    if(transform.childCount>=cubeBlockLimit)
+                    IsActive = false;
+                    break;
+                case ObjectTypes.CubePickup:
+                    playerCont.AddCubes(cubesToAdd);
+                    Destroy(gameObject);
+                    break;
+            }
+        }
     }
 }
--- Assets/Akshansh/Scripts/PlayerController.cs
+++ Assets/Akshansh/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3f,minXlimit,maxXlimit;
+    [SerializeField] float moveSpeed = 3f,minXlimit,maxXlimit,cubeHeight = 1;
+    [SerializeField] GameObject cubPref;
+    [SerializeField] int startingCubeCount = 1;
+
+    private void Start()
+    {
+        AddCubes(startingCubeCount - 1);//1 cube is added by default
+    }
 
     //Moves player on x axis in givn direction (not normalized due to random touch intensity)
     public void MovePlayer(float _dir)
@@ -15,4 +21,35 @@ public class PlayerController : MonoBehaviour
         transform.position = _tempPos;
     }
 
+    //generate cubes below already existing cubes based on ammount
+    public void AddCubes(int _ammount)
+    {
+        int _tempChildCount = transform.childCount;
+        List<Rigidbody> _tempChilds = new List<Rigidbody>();
+        //disable rb for avoiding physics error if any
+        for(int i = 0;i<_tempChildCount;i++)
+        {
+            _tempChilds.Add(transform.GetChild(i).GetComponent<Rigidbody>());
+            _tempChilds[i].isKinematic = true;
+        }
+
+        //increment existing cubes and add new one below it
+        for(int i=0; i < _ammount; i++)
+        {
+            //update existing cubes
+            foreach(Rigidbody _tempChild in _tempChilds)
+            {
+                _tempChild.transform.position += new Vector3(0, cubeHeight, 0);
+            }
+            Rigidbody _tempRb = Instantiate(cubPref, transform).GetComponent<Rigidbody>();
+            _tempRb.isKinematic = true;
+            _tempChilds.Add(_tempRb);
+        }
+        //enable physics again once added
+        foreach(var v in _tempChilds)
+        {
+            v.isKinematic = false;
+        }
+
+    }
 }
